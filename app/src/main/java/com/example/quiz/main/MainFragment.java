3746f9465f8946e641af9f6482cbe48a553e0aca
RavenRav <akilaia@inbox.ru>
2020-03-06 17:14:45 +0300

    верстка первого фрагмента

--- app/src/main/java/com/example/quiz/main/MainFragment.java
+++ app/src/main/java/com/example/quiz/main/MainFragment.java
@@ -3,19 +3,27 @@ package com.example.quiz.main;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SeekBar;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.example.quiz.R;
+import com.example.quiz.utils.SimpleOnSeekBarChangeListener;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class MainFragment extends Fragment {
+    private SeekBar seekBar;
+    private TextView textView;
 
     public static MainFragment newInstance() {
         return new MainFragment();
@@ -34,4 +42,20 @@ public class MainFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        seekBar = view.findViewById(R.id.seek_bar_fm);
+        textView = view.findViewById(R.id.q_amount);
+
+        seekBar.setOnSeekBarChangeListener(new SimpleOnSeekBarChangeListener() {
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                super.onStopTrackingTouch(seekBar);
+                textView.setText(String.valueOf(seekBar.getProgress()));
+            }
+        });
+
+
+    }
 }
