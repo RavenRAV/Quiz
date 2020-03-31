fa58773c1e9a1ededef175fc8b809cec25727adb
RavenRav <akilaia@inbox.ru>
2020-03-31 04:50:50 +0300

    передача параметров 3)4

--- app/src/main/java/com/example/quiz/quiz/QuizActivity.java
+++ app/src/main/java/com/example/quiz/quiz/QuizActivity.java
@@ -1,6 +1,7 @@
 package com.example.quiz.quiz;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.content.Context;
 import android.content.Intent;
@@ -9,14 +10,24 @@ import android.os.Bundle;
 import com.example.quiz.R;
 
 public class QuizActivity extends AppCompatActivity {
+    private static final String EXTRA_AMOUNT = "amount";
+    private static final String EXTRA_CATEGORY = "category";
+    private static final String EXTRA_DIFFICULTY = "difficulty";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        ViewModelProviders.of(this).get(QuizViewModel.class).init(
+                10, 2, "difficulty"
+        );
     }
-    public static void start(Context context){
+    public static void start(Context context, int amount, String category, String difficulty){
         Intent intent = new Intent(context, QuizActivity.class);
+        intent.putExtra(EXTRA_AMOUNT, amount);
+        intent.putExtra(EXTRA_CATEGORY, category);
+        intent.putExtra(EXTRA_DIFFICULTY, difficulty);
         context.startActivity(intent);
     }
 }
--- app/src/main/java/com/example/quiz/quiz/QuizViewModel.java
+++ app/src/main/java/com/example/quiz/quiz/QuizViewModel.java
@@ -3,4 +3,6 @@ package com.example.quiz.quiz;
 import androidx.lifecycle.ViewModel;
 
 public class QuizViewModel extends ViewModel {
+    public void init(int amount, Integer category, String difficulty) {
+    }
 }
